b359d344e1f910b84383fcfa6c134c2e6c829b39
Elena Creacea <elena.cracea@wearepropeople.com>
2013-07-11 16:33:18 +0300

    staff layout

--- sites/all/themes/slac/plugins/layouts/staff_resources/staff-resources.tpl.php
+++ sites/all/themes/slac/plugins/layouts/staff_resources/staff-resources.tpl.php
@@ -17,16 +17,9 @@
 <div class="panel-display staff-resources clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
   <div class="panel-panel staff-top">
-	  <div class="panel-panel staff-top-one">
-      <?php if ($content['staffresourcesone']): ?>
-        <div class="inside"><?php print $content['staffresourcesone']; ?></div>
+      <?php if ($content['staffresourcestop']): ?>
+        <div class="inside"><?php print $content['staffresourcestop']; ?></div>
       <?php endif ?>
-	  </div>
-	  <div class="panel-panel staff-top-two">
-      <?php if ($content['staffresourcestwo']): ?>
-        <div class="inside"><?php print $content['staffresourcestwo']; ?></div>
-      <?php endif ?>
-	  </div>
   </div>
 
   <div class="panel-panel staff-main">
