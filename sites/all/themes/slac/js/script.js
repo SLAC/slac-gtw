cfa8a95bce8f26cb49e287d84dd7bd589e8703a2
Elena Creacea <elena.cracea@wearepropeople.com>
2013-07-29 15:58:01 +0300

    task #6496, comment #5  pt.4

--- sites/all/themes/slac/js/script.js
+++ sites/all/themes/slac/js/script.js
@@ -62,22 +62,10 @@
 	  }
 	}
 
-
-
-
-	// Drupal.behaviors.staffMenu = {
-	//   attach: function (context, settings) {
-
-	//   	$(window).resize(function() {
-	// 			if (Modernizr.mq('(max-width: 900px)')) {		
-	// 				$('.pane-quicktabs-staff-resources .quicktabs-wrapper > .item-list li a').click(function(){
-	// 				  var $destination = $(this).closest('li');
-	// 				  $('.pane-quicktabs-staff-resources .quicktabs_main').insertAfter($destination).show(1);
-	// 				});
-	// 			}
-	// 		});
-	//   }
-	// }
-
+	Drupal.behaviors.listOddClass = {
+	  attach: function (context, settings) {
+		$('.brown-bodered-white .menu li').filter(function(index) {return index % 2 == 0}).addClass('odd');
+	  }
+	}
 
 })(jQuery, Drupal, this, this.document);
