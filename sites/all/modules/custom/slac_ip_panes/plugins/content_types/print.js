55b18b8289c6d09792b049dc18ef7762b85da4ba
kepford <bob@bobkepford.com>
2015-03-24 10:47:23 -0700

    Enable v_featured_news_pane feature
    * Fix print.js synax mistake
    
    node/59972

--- sites/all/modules/custom/slac_ip_panes/plugins/content_types/print.js
+++ sites/all/modules/custom/slac_ip_panes/plugins/content_types/print.js
@@ -7,15 +7,15 @@
   'use strict';
 
   Drupal.behaviors.slacIpPanesPrint = {
-     attach: function(context) {
-       if (context === document) {
-        $('.js-print-button').once('slacIpPanesPrint', function() {
+    attach: function(context) {
+      if (context === document) {
+        $('.js-print-button').once('slacIpPanesPrint', function () {
           var $link = $('<a/>')
-              .attr('href', 'javascript:window.print();')
-              .text('Print');
-           $(this).append($link);
-         }
-       }
-     }
-   };
+          .attr('href', 'javascript:window.print();')
+          .text('Print');
+          $(this).append($link);
+        });
+      }
+    }
+  };
 })(jQuery);
