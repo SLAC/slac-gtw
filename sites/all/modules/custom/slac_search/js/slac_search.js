10bc5feb79d8e4dbb5c4e73f94297ef44575eba4
Peter Mallett <peter.mallett@mediacurrent.com>
2015-04-06 13:49:14 -0400

    Add dynamic search placeholder text, fix default
    node/60339

--- sites/all/modules/custom/slac_search/js/slac_search.js
+++ sites/all/modules/custom/slac_search/js/slac_search.js
@@ -1,9 +1,29 @@
+/**
+ * @file
+ * Search form custom behaviors.
+ */
+
 (function ($) {
   'use strict';
 
+  /**
+   * Applies the Selectize plugin and adds dynamic placeholder text to the
+   * search input.
+   */
   Drupal.behaviors.slac_search = {
     attach: function(context, settings) {
-      $('#search-block-form select', context).selectize();
+      var placeholders = {
+        'default': Drupal.t('Search SLAC Today'),
+        'people': Drupal.t('Search People'),
+        'enterprise': Drupal.t('Search SLAC Web')
+      };
+      var $searchInput = $('#search-block-form .form-item-search-block-form input');
+
+      $('#search-block-form select', context).selectize({
+        onChange: function(value) {
+          $searchInput.attr('placeholder', placeholders[value]);
+        }
+      });
     }
   };
 
