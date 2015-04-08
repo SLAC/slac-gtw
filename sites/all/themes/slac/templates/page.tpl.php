debc50443ba603052e816110567d4169356a283e
Mario Hernandez <mario.hernandez@mediacurrent.com>
2015-04-08 15:16:11 -0700

    Uses SVG for logo
    Removed img tag for logo in page.tpl.php and instead used background
    image for homelink/logo.  Used SVG as primary image and png as fallback.
    
    mario h.
    node/60522

--- sites/all/themes/slac/templates/page.tpl.php
+++ sites/all/themes/slac/templates/page.tpl.php
@@ -77,7 +77,7 @@
       
     <?php if ($logo): ?>
       <div class="logo">
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"></a>
       </div>
     <?php endif; ?>
 
