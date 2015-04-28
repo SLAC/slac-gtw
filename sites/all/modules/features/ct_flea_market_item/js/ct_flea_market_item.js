68049ef6bb9f2fb48343ce9ddad509d28af7d5ee
Peter Mallett <peter.mallett@mediacurrent.com>
2015-04-28 16:12:43 -0400

    Add custom Flag confirmation form, behavriors
    node/60821

---
+++ sites/all/modules/features/ct_flea_market_item/js/ct_flea_market_item.js
@@ -0,0 +1,57 @@
+(function ($) {
+  'use strict';
+
+  Drupal.behaviors.fleaMarketConfirm = {
+    attach: function (context, settings) {
+      $('span.flag-wrapper a.flag-link-toggle', context).once('fleaMarketConfirm', function () {
+        var confirmDialog;
+        var flagLink = $(this);
+
+        var href = flagLink.attr('href');
+        var nid = /flea_market_inappropriate\/(\d+)/.exec(href)[1];
+
+        // Unbind the Flag click handler from the flag link and show the confirm
+        // form instead.
+        $(this)
+          .unbind('click')
+          .bind('click', function (e) {
+            e.preventDefault();
+            confirmDialog.toggle();
+          })
+          .after(Drupal.theme('FleaMarketConfirm', href, nid));
+
+        // Close the confirm dialog if either close button is clicked.
+        confirmDialog = $('#flea_market_flag_confirm', context);
+        confirmDialog.find('.close').bind('click', function (e) {
+          e.preventDefault();
+          confirmDialog.toggle();
+        });
+
+        // Attach the flag toggle to the custom submit link.
+        confirmDialog.find('.submit').addClass('flag flag-action flag-link-toggle');
+        Drupal.attachBehaviors(confirmDialog);
+      });
+    }
+  };
+
+  /**
+   * Provide the HTML to create the modal dialog.
+   */
+  Drupal.theme.FleaMarketConfirm = function (href, nid) {
+    var html = '';
+
+    html += '<div id="flea_market_flag_confirm" style="display:none;">';
+    html += '  <h4>Report Flea Market Ad as Inappropriate</h4>';
+    html += '  <button class="close">X</button>';
+    html += '  <p>Flagged ad content will be reviewed by the Office of Communications to determine whether SLAC\'s appropriate use policy of the web have been violated.</p>';
+    html += '  <p class="flag-wrapper flag-flea-market-inappropriate flag-flea-market-inappropriate-' + nid + '">';
+    html += '    <a href="' + href + '" class="submit">Submit</a>';
+    html += '    <span>or</span>';
+    html += '    <button class="close">I\'ve changed my mind, close dialog box</button>';
+    html += '  </p>';
+    html += '</div>';
+
+    return html;
+  };
+
+})(jQuery);
