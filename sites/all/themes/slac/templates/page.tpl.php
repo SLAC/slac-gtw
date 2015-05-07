ec5aff2474a4660c87572644d890a576aa230a53
Mario Hernandez <mario.hernandez@mediacurrent.com>
2015-05-07 06:59:26 -0700

    Cleans up footer region's markup from page.tpl.php
    To avoid footer logos downloading for all pages and all devices
    I changed the structure and modified functionality so logos are
    served by CSS on only the appropriate device logos are served based
    on breakpoints.
    
    mario h.
    node/61483

--- sites/all/themes/slac/templates/page.tpl.php
+++ sites/all/themes/slac/templates/page.tpl.php
@@ -135,12 +135,8 @@
     <div class="footer-inner">
       <?php print render($page['footer']); ?>
       <div class="footer-logos">
-        <a href="http://science.energy.gov/"><img src="/sites/all/themes/slac/images/footer-logo-energy.jpg" alt=""></a>
-        <a href="http://www.stanford.edu/"><img src="/sites/all/themes/slac/images/footer-logo-stanford.jpg" alt=""></a>
-      </div>
-      <div class="footer-logos mobile">
-        <a href="http://science.energy.gov/"><img src="/sites/all/themes/slac/images/footer-logo-energy-small.jpg" alt=""></a>
-        <a href="http://www.stanford.edu/"><img src="/sites/all/themes/slac/images/footer-logo-stanford-small.jpg" alt=""></a>
+        <a class="dept-of-energy" href="http://science.energy.gov/"><span>US Department of Energy</span></a>
+        <a class="stanford-u" href="http://www.stanford.edu/"><span>Stanford University</span></a>
       </div>
     </div>
   </div>
