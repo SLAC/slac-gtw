07f70cda33fc5ca77e4948ea128a5fa1dc0ac2a3
Peter Mallett <peter.mallett@mediacurrent.com>
2015-07-23 10:25:55 -0400

    Fix error caused by Single Day end time entry
    node/63894

--- sites/all/modules/custom/slac_ip_event_date/js/slac_ip_event_date.js
+++ sites/all/modules/custom/slac_ip_event_date/js/slac_ip_event_date.js
@@ -19,9 +19,11 @@
 
       var $form = $('.node-form', context);
       var $singleDay = $form.find('input.form-checkbox[id^="edit-field-event-date"]');
-      var $value = $form.find('input[id^="edit-field-event-date-"][id*="value-"]');
-      var $value2 = $form.find('input[id^="edit-field-event-date-"][id*="value2-"]');
-      var msgs = {};
+      // These behaviors are only concerned with the dates, not the times,
+      // so $value, $value2 are only the first input in each field.
+      var $value = $form.find('input[id^="edit-field-event-date-"][id*="value-"]').first();
+      var $value2 = $form.find('input[id^="edit-field-event-date-"][id*="value2-"]').first();
+      var messages = {};
 
       // The field controlling 'Single Day' is really the 'All Day' toggle.
       // Set the checked value back to what it should be for submission.
@@ -32,25 +34,33 @@
 
       // If 'Single Day' is checked, set the "to" date to match the start date.
       $singleDay.change(function (e) {
-        var firstVal = $value.first().val();
-        var secondVal = $value2.first().val();
+        var startVal = $value.val();
+        var endVal = $value2.val();
 
         if (e.target.checked) {
-          if (firstVal !== secondVal) {
-            $value2.first().val(firstVal);
+          if (startVal !== endVal) {
+            $value2.val(startVal);
 
-            showTempMessage(msgs.to_date_reset, 'fast', 2000);
+            showTempMessage(messages.to_date_reset, 'fast', 2000);
           }
 
           // Disable the "to" date.
-          $value2.first().parent().hide();
+          $value2.parent().hide();
 
           // Hide the "Single Day" help message.
-          msgs.all_day.slideUp();
+          messages.all_day.slideUp();
         }
         else {
-          $value2.first().parent().show();
-          msgs.all_day.slideDown();
+          $value2.parent().show();
+          messages.all_day.slideDown();
+        }
+      });
+
+      // If the start date is changed while the "Single Day" option is checked,
+      // fill in the "to" date to match.
+      $value.change(function () {
+        if ($singleDay.prop('checked')) {
+          $value2.val($value.val());
         }
       });
 
@@ -60,21 +70,21 @@
         if ($value2.val() !== $value.val() && $singleDay.prop('checked')) {
           $singleDay.prop('checked', false).change();
 
-          showTempMessage(msgs.single_day, 'fast', 5000);
+          showTempMessage(messages.single_day, 'fast', 5000);
         }
       });
 
       // Add status/help messages to show/hide if 'Single Day' is changed.
-      msgs.all_day = $('<div class="messages status">' +
+      messages.all_day = $('<div class="messages status">' +
           Drupal.t('Only "Single Day" dates may have a start/end time. This entry will be considered "All Day" in each calendar item.') +
           '</div>').hide();
-      msgs.single_day = $('<div class="messages warning">' +
+      messages.single_day = $('<div class="messages warning">' +
           Drupal.t('You changed a "Single Day" entry to span multiple days, if you need a multi-day entry with start/end times, please enter as "Single Day" and then clone the entry.') +
           '</div>').hide();
-      msgs.to_date_reset = $('<div class="messages warning">' +
+      messages.to_date_reset = $('<div class="messages warning">' +
           Drupal.t('The "to" date has been reset to match the start date since "Single Day" was checked.') +
           '</div>').hide();
-      $.each(msgs, function (name, $el) {
+      $.each(messages, function (name, $el) {
         $singleDay.parent().before($el);
       });
 
@@ -84,7 +94,7 @@
 
       // After running on-attach, if "Single Day", show the appropriate message immediately.
       if (!$singleDay.prop('checked')) {
-        msgs.all_day.show();
+        messages.all_day.show();
       }
 
     }
