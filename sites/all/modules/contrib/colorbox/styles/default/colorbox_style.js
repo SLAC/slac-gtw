839aa178e96271baf8b8676d698ed2ccfeb08e6b
Vincent Flesouras <x.algorithm@gmail.com>
2016-04-22 16:30:44 +0000

    Security updates up until 4-21-2016

--- sites/all/modules/contrib/colorbox/styles/default/colorbox_style.js
+++ sites/all/modules/contrib/colorbox/styles/default/colorbox_style.js
@@ -15,30 +15,6 @@ Drupal.behaviors.initColorboxDefaultStyle = {
       else {
         $('#cboxTitle', context).hide();
       }
-      var colorbox_original_image = Drupal.settings.colorbox.colorbox_original_image;
-      if(colorbox_original_image == '1') {
-        function addLink() {
-          if ($('#cboxDownload').length) {
-            $('#cboxDownload').remove();
-          }
-          var fullHref = $('#cboxLoadedContent > img').attr('src').replace(/styles\/large\/public\//,'');
-          var fullLink = $('<a/>');
-          fullLink.attr('href', fullHref);
-          fullLink.attr('target', 'new');
-          fullLink.attr('title', 'Right click to download');
-          fullLink.addClass("download_link");
-          $('#cboxClose').before(fullLink);
-          $('.download_link').wrap('<div id="cboxDownload"></div>');
-        }
-
-        if ($('#cboxLoadedContent > img').attr('src')) {
-          addLink();
-        } else {
-          if ($('#cboxDownload').length) {
-            $('#cboxDownload').remove();
-          }
-        }
-      }
     });
   }
 };
