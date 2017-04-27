6fad483be2733fd1c5a13f7b2001063e4ba855d2
Vincent Flesouras <x.algorithm@gmail.com>
2017-04-27 10:04:59 -0700

    Adding client requested language

--- sites/all/modules/features/slac_igp_email/slac_igp_email.tpl.php
+++ sites/all/modules/features/slac_igp_email/slac_igp_email.tpl.php
@@ -6,7 +6,9 @@
  */
 ?>
 <?php if (empty($featured_news) && empty($news)): ?>
-<span>No announcements were added to SLAC Today yesterday. Please visit the SLAC Today home page to see the lab's most recent news and updates.</span>
+    <p>This daily email newsletter consists of news and announcements that were published to the SLAC Today home page since the previous day’s newsletter. No news items were published yesterday.</p>
+    <p>Please visit the <a href="https://intranet.slac.stanford.edu/">SLAC Today home page</a> to see the lab’s most recent news and information.</p>
+    <p>If you would like to submit an announcement to publish in SLAC Today, please visit the <a href="https://intranet.slac.stanford.edu/about-slac-today">About SLAC Today page</a> for guidance. If you would like to suggest a story idea, please fill out our <a href="https://intranet.slac.stanford.edu/suggest-a-story">web form</a> or email <a href="mailto:communications@slac.stanford.edu">Communications</a>.</p>
 <?php endif; ?>
 <div id="featured_news">
 <?php foreach($featured_news as $featured): ?>
